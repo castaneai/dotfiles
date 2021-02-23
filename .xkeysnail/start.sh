74a8f2eaf13141da67e41dced5f341f029ec9fe3
castaneai <castaneai@castaneai.net>
2021-02-24 02:06:02 +0900

    MX Keys -> Logitech Wireless Device ...? (+_+)

--- .xkeysnail/start.sh
+++ .xkeysnail/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ -x /usr/local/bin/xkeysnail ]; then
     xhost +SI:localuser:xkeysnail
-    device=`sudo -u xkeysnail python3 -c "import evdev; print([device.path for device in [evdev.InputDevice(path) for path in evdev.list_devices()] if 'MX Keys' in device.name][0])"`
+    device=`sudo -u xkeysnail python3 -c "import evdev; print([device.path for device in [evdev.InputDevice(path) for path in evdev.list_devices()] if 'Logitech Wireless' in device.name][0])"`
     sudo -u xkeysnail DISPLAY=$DISPLAY /usr/local/bin/xkeysnail -q $HOME/.xkeysnail/config.py --device $device &
 fi
 
