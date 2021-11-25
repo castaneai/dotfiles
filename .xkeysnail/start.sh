7c27f08a0cce3780741e00a7a05450db98ab0e59
castaneai <castaneai@castaneai.net>
2021-11-25 15:26:19 +0900

    update xkeysnail device (for Logicool MX Keys mini)

--- .xkeysnail/start.sh
+++ .xkeysnail/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ -x /usr/local/bin/xkeysnail ]; then
     xhost +SI:localuser:root
-    device=`python3 -c "import evdev; print([device.path for device in [evdev.InputDevice(path) for path in evdev.list_devices()] if 'MX Keys' in device.name][0])"`
-    /usr/local/bin/xkeysnail /home/castaneai/.xkeysnail/config.py --device $device
+    # device=`python3 -c "import evdev; print([device.path for device in [evdev.InputDevice(path) for path in evdev.list_devices()] if 'MX Keys' in device.name][0])"`
+    /usr/local/bin/xkeysnail /home/castaneai/.xkeysnail/config.py
 fi
