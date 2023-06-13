00268a0d69775045d5a72091640c3e831c0e5120
castaneai <castaneai@castaneai.net>
2023-06-13 17:52:23 +0900

    update vscode config

--- install.sh
+++ install.sh
@@ -8,9 +8,14 @@ for f in "${dotfiles[@]}"; do
     ln -nsf "${DOTFILES_DIR}/${f}" ~/${f}
 done
 
-mkdir -p ~/.config/Code/User
-ln -nsf "${DOTFILES_DIR}/.config/Code/User/settings.json" ~/.config/Code/User/settings.json
-ln -nsf "${DOTFILES_DIR}/.config/Code/User/keybindings.json" ~/.config/Code/User/keybindings.json
+OS_CONFIG_DIR="${HOME}/.config"
+if [ "$(uname)" == "Darwin" ]; then
+    OS_CONFIG_DIR="${HOME}/Library/Application Support"
+fi
+
+mkdir -p "${OS_CONFIG_DIR}/Code/User"
+ln -nsf "${DOTFILES_DIR}/.config/Code/User/settings.json" "${OS_CONFIG_DIR}/Code/User/settings.json"
+ln -nsf "${DOTFILES_DIR}/.config/Code/User/keybindings.json" "${OS_CONFIG_DIR}/Code/User/keybindings.json"
 
 mkdir -p ~/.config/xfce4/terminal
 ln -nsf "${DOTFILES_DIR}/.config/xfce4/terminal/accels.scm" ~/.config/xfce4/terminal/accels.scm
@@ -19,11 +24,5 @@ ln -nsf "${DOTFILES_DIR}/.config/xfce4/terminal/terminalrc" ~/.config/xfce4/term
 mkdir -p ~/.config/gitui
 ln -nsf "${DOTFILES_DIR}/.config/gitui/key_bindings.ron" ~/.config/gitui/key_bindings.ron
 
-
-OS_CONFIG_DIR="${HOME}/.config"
-if [ "$(uname)" == "Darwin" ]; then
-    OS_CONFIG_DIR="${HOME}/Library/Application Support"
-fi
 mkdir -p "${OS_CONFIG_DIR}/k9s"
 ln -nsf "${DOTFILES_DIR}/.config/k9s/plugin.yml" "${OS_CONFIG_DIR}/k9s/plugin.yml"
-
