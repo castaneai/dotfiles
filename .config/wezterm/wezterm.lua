bb561fe78cbaf5ba8e43f56f743511bbcd32b8cf
castaneai <castaneai@castaneai.net>
2024-07-23 16:55:55 +0900

    wezterm: Do not open mailto: link on click

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,4 +8,12 @@ config.color_scheme = 'ayu'
 config.send_composed_key_when_left_alt_is_pressed = true
 config.front_end = 'WebGpu'
 
+wezterm.on('open-uri', function(window, pane, uri)
+  -- Do not open `mailto:` link
+  local start, match_end = uri:find 'mailto:'
+  if start == 1 then
+    return false
+  end
+end)
+
 return config
